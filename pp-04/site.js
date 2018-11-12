73577d6df5e93311cd24df7deeac0cef376ea1ed
blueshysky <xionghuih745@gmail.com>
2018-11-12 15:19:48 -0600

    production problem completed

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,12 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+  //document.querySelector()
+
+});
+
+
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+    doorbell.play();
+};
